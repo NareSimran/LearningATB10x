e7567baaf868c084acfd3d194fd51041ae0e64ac
NareSimran <nare.simran1996@gmail.com>
2025-01-11 19:25:59 +0530

    Added new files

---
+++ src/Lab005_variables.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class Lab005_variables {
+    public static void main(String[] args) {
+       int a=10;
+       //data_type variable_name=value;
+       // variable_name=identifier;
+        //variable_value=literal;
+        int age=89;
+        age=age+1;
+        age=76;
+        System.out.println(age);
+    }
+}
---
+++ src/Lab_006_task.java
@@ -0,0 +1,18 @@
+public class Lab_006_task {
+    public static void main(String[] args) {
+        int num=9;
+        System.out.printf("Table of 9 is :9*1=%d\n",num*1);
+        System.out.printf("9*1=%d\n",num*2);
+        System.out.printf("9*1=%d\n",num*3);
+        System.out.printf("9*1=%d\n",num*4);
+        System.out.printf("9*1=%d\n",num*5);
+        System.out.printf("9*1=%d\n",num*6);
+        System.out.printf("9*1=%d\n",num*7);
+        System.out.printf("9*1=%d\n",num*8);
+        System.out.printf("9*1=%d\n",num*9);
+        System.out.printf("9*1=%d\n",num*10);
+
+
+
+    }
+}
