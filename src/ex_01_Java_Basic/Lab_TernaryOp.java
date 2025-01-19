67bffb7776753c1b25c1f95f768eda787eb8a876
NareSimran <nare.simran1996@gmail.com>
2025-01-19 18:15:50 +0530

    Added 19 Jan task

--- src/ex_01_Java_Basic/Lab_TernaryOp.java
+++ src/ex_01_Java_Basic/Lab_TernaryOp.java
@@ -2,6 +2,18 @@ package ex_01_Java_Basic;
 
 public class Lab_TernaryOp {
     public static void main(String[] args) {
+       //Write a program that calculates and displays the letter grade for a given numerical score (e.g., A, B, C, D, or F) based on the following grading scale:
+        //
+        //A: 90-100
+        //
+        //B: 80-89
+        //
+        //C: 70-79
+        //
+        //D: 60-69
+        //
+        //F: 0-59
+
         int marks=91;
         String Grade=
                 (marks>=90&&marks<=100)?"A":
