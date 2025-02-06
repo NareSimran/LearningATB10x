f60a01c909812b0f93b9fed9a4168c53e33c1ec2
NareSimran <nare.simran1996@gmail.com>
2025-02-06 12:03:36 +0530

    Added 05 Feb task -For and while loop programs

---
+++ src/Task_02Feb_StringFunctions/Task_002.java
@@ -0,0 +1,11 @@
+package Task_02Feb_StringFunctions;
+
+public class Task_002 {
+    public static void main(String[] args) {
+        String str="    hey hello   ";
+        System.out.println(str.trim());
+
+        String name3 = "    Simran Shree     ";
+        System.out.println(name3.trim());
+    }
+}
---
+++ src/Task_05Feb_for_while_loops/Task_004.java
@@ -0,0 +1,19 @@
+package Task_05Feb_for_while_loops;
+
+
+
+public class Task_004 {
+    //Print the sum of first 10 natural nos
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=1;i<=10;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.println(sum);
+
+
+
+
+    }
+}
